9d0e4e35dc5c3f4fd8557da399f4b9205496c02a
maba-2001 <matteo.ballegeer@ugent.be>
2026-03-17 18:43:16 +0000

    Deploying to gh-pages from @ maba-2001/maba-2001.github.io@10026e1750be48e70972cf2544b9bf6fdad1c70a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "maba-2001/resume",
+    "data-repo": "maba-2001/maba-2001.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
